6064fb5e619e1d3c4f5fbcd5d5d4f81538dd62d5
nsonanh <sonanhandynguyen@gmail.com>
2017-06-18 16:19:25 +0700

    [feature-api-doc-impl] Implement api docs

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ let bodyParser  = require('body-parser');
 let morgan      = require('morgan');
 let config      = require('config'); //we load the db location from the JSON files
 
+// Serving static files from "public" folder
+app.use(express.static('public'));
 
 // CALL LOCAL LIBRARIES
 // call lang to get accepted language from request
@@ -52,11 +54,63 @@ router.get('/', function(req, res) {
 router.route('/danhngon')
 
     // create a danhngon (accessed at POST http://localhost:8080/api/danhngon)
+    /**
+     * @api {post} /api/danhngon Register a new danhngon
+     * @apiVersion 1.0.0
+     * @apiGroup Danhngon
+     * @apiParam {String} content danhngon content
+     * @apiParam {String} author danhngon author
+     * @apiParam {String} language danhngon language
+     * @apiParamExample {json} Input
+     *    {
+     *      "content": "A smile is the universal welcome.",
+     *      "author": "Max Eastman",
+     *      "language": "en"
+     *    }
+     * @apiSuccess {Number} _id danhngon id
+     * @apiSuccess {String} content danhngon content
+     * @apiSuccess {String} author danhngon author
+     * @apiSuccess {String} language danhngon language
+     * @apiSuccess {Date} created_at Register date
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "_id": 594634907c371c3e209e3446,
+     *      "content": "A smile is the universal welcome.",
+     *      "author": "Max Eastman",
+     *      "language": "en",
+     *      "created_at": "2017-06-18T08:06:40.926Z"
+     *    }
+     * @apiErrorExample {json} Register error
+     *    HTTP/1.1 500 Internal Server Error
+     */
     .post(function(req, res) {
         requestHandler.handlePost(req, res);
     })
 
     // get all the danhngon (accessed at GET http://localhost:8080/api/danhngon)
+    /**
+     * @api {get} /api/danhngon List all danhngon
+     * @apiVersion 1.0.0
+     * @apiGroup Danhngon
+     * @apiSuccess {Object[]} danhngon Danhngon's list
+     * @apiSuccess {Number} danhngon._id danhngon id
+     * @apiSuccess {String} danhngon.content danhngon content
+     * @apiSuccess {String} danhngon.author danhngon author
+     * @apiSuccess {String} danhngon.language danhngon language
+     * @apiSuccess {Date} danhngon.created_at Register's date
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    [{
+     *      "_id": 594634907c371c3e209e3446,
+     *      "content": "A smile is the universal welcome.",
+     *      "author": "Max Eastman",
+     *      "language": "en",
+     *      "created_at": "2017-06-18T08:06:40.926Z"
+     *    }]
+     * @apiErrorExample {json} List error
+     *    HTTP/1.1 500 Internal Server Error
+     */
     .get(function(req, res) {
         requestHandler.handleGet(req, res);
     });
@@ -66,15 +120,62 @@ router.route('/danhngon')
 router.route('/danhngon/random')
 
     // get the random danhngon (accessed at GET http://localhost:8080/api/danhngon/random)
+    /**
+     * @api {get} /api/danhngon/random Find a random danhngon
+     * @apiVersion 1.0.0
+     * @apiGroup Danhngon
+     * @apiSuccess {Number} danhngon._id danhngon id
+     * @apiSuccess {String} danhngon.content danhngon content
+     * @apiSuccess {String} danhngon.author danhngon author
+     * @apiSuccess {String} danhngon.language danhngon language
+     * @apiSuccess {Date} danhngon.created_at Register's date
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "_id": 594634907c371c3e209e3446,
+     *      "content": "A smile is the universal welcome.",
+     *      "author": "Max Eastman",
+     *      "language": "en",
+     *      "created_at": "2017-06-18T08:06:40.926Z"
+     *    }
+     * @apiErrorExample {json} danhngon not found
+     *    HTTP/1.1 404 Not Found
+     * @apiErrorExample {json} Find error
+     *    HTTP/1.1 500 Internal Server Error
+     */
     .get(function(req, res) {
         requestHandler.handleGetRandom(req, res);
     });
 
-// on routes that end in /danhngon/random
+// on routes that end in /danhngon/random/:tramslatedLanguage
 // ----------------------------------------------------
 router.route('/danhngon/random/:translatedlanguage')
 
-    // get the random danhngon (accessed at GET http://localhost:8080/api/danhngon/random)
+    // get the random translated danhngon (accessed at GET http://localhost:8080/api/danhngon/random)
+    /**
+     * @api {get} /api/danhngon/random/:language Find a random translated danhngon
+     * @apiVersion 1.0.0
+     * @apiGroup Danhngon
+     * @apiParam {String} language language to translate to (in ISO code e.g. "vi")
+     * @apiSuccess {Number} danhngon._id danhngon id
+     * @apiSuccess {String} danhngon.content danhngon content
+     * @apiSuccess {String} danhngon.author danhngon author
+     * @apiSuccess {String} danhngon.language danhngon language
+     * @apiSuccess {Date} danhngon.created_at Register's date
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "_id": 594634907c371c3e209e3446,
+     *      "content": "Một nụ cười là sự chào đón phổ quát.",
+     *      "author": "Max Eastman",
+     *      "language": "en",
+     *      "created_at": "2017-06-18T08:06:40.926Z"
+     *    }
+     * @apiErrorExample {json} danhngon not found
+     *    HTTP/1.1 404 Not Found
+     * @apiErrorExample {json} Find error
+     *    HTTP/1.1 500 Internal Server Error
+     */
     .get(function(req, res) {
         requestHandler.handleGetRandomWithLang(req, res);
     });
@@ -84,16 +185,77 @@ router.route('/danhngon/random/:translatedlanguage')
 router.route('/danhngon/:danhngon_id')
 
     // get the danhngon with that id (accessed at GET http://localhost:8080/api/danhngon/:danhngon_id)
+    /**
+     * @api {get} /api/danhngon/:id Find a danhngon with id
+     * @apiVersion 1.0.0
+     * @apiGroup Danhngon
+     * @apiParam {Number} _id danhngon id
+     * @apiSuccess {Number} danhngon._id danhngon id
+     * @apiSuccess {String} danhngon.content danhngon content
+     * @apiSuccess {String} danhngon.author danhngon author
+     * @apiSuccess {String} danhngon.language danhngon language
+     * @apiSuccess {Date} danhngon.created_at Register's date
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "_id": 594634907c371c3e209e3446,
+     *      "content": "A smile is the universal welcome.",
+     *      "author": "Max Eastman",
+     *      "language": "en",
+     *      "created_at": "2017-06-18T08:06:40.926Z"
+     *    }
+     * @apiErrorExample {json} danhngon not found
+     *    HTTP/1.1 404 Not Found
+     * @apiErrorExample {json} Find error
+     *    HTTP/1.1 500 Internal Server Error
+     */
     .get(function(req, res) {
         requestHandler.handleGetWithID(req, res);
     })
 
     // update the danhngon with this id (accessed at PUT http://localhost:8080/api/danhngon/:danhngon_id)
+    /**
+     * @api {put} /api/danhngon/:id Update a danhngon
+     * @apiVersion 1.0.0
+     * @apiGroup Danhngon
+     * @apiParam {id} _id danhngon id
+     * @apiParam {String} content danhngon content
+     * @apiParam {String} author danhngon author
+     * @apiParam {String} language danhngon language
+     * @apiParamExample {json} Input
+     *    {
+     *      "_id": 594634907c371c3e209e3446,
+     *      "content": "A smile is the universal welcome.",
+     *      "author": "Max Eastman",
+     *      "language": "en"
+     *    }
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "_id": 594634907c371c3e209e3446,
+     *      "content": "A smile is the universal welcome.",
+     *      "author": "Max Eastman",
+     *      "language": "en",
+     *      "created_at": "2017-06-18T08:06:40.926Z"
+     *    }
+     * @apiErrorExample {json} Register error
+     *    HTTP/1.1 500 Internal Server Error
+     */
     .put(function(req, res) {
         requestHandler.handlePut(req, res);
     })
 
     // delete the danhngon with this id (accessed at DELETE http://localhost:8080/api/danhngon/:danhngon_id)
+    /**
+     * @api {delete} /api/danhngon/:id Remove a danhngon
+     * @apiVersion 1.0.0
+     * @apiGroup Danhngon
+     * @apiParam {id} _id danhngon id
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 204 No Content
+     * @apiErrorExample {json} Delete error
+     *    HTTP/1.1 500 Internal Server Error
+     */
     .delete(function(req, res) {
         requestHandler.handleDelete(req, res);
     });
