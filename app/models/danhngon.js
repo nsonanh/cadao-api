0df40793cfe39456becd61ad84927fe604d5f544
nsonanh <sonanhandynguyen@gmail.com>
2017-06-19 23:29:53 +0700

    [feature-fix-server-crashes] Fix server crashes

--- app/models/danhngon.js
+++ app/models/danhngon.js
@@ -7,7 +7,7 @@ var DanhngonSchema   = new Schema({
     content:    { type: String, required: true },
     author:     { type: String, required: true },
     language:   { type: String, required: true },
-    createdAt:  { type: Date, default: Date.now }
+    createdAt:  { type: Date, required: true, default: Date.now }
 },
 {
     collection: 'Danhngon',
