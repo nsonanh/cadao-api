ed0ecd04386af04cdd0632f4df0bd137e09acff3
nsonanh <sonanhandynguyen@gmail.com>
2017-06-26 22:37:54 +0700

    [feature-get-language-impl] Update docs

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-06-26T15:25:11.992Z",
+    "time": "2017-06-26T15:37:00.253Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
